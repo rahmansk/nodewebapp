13d2285ddd526247fa0686e57219a7ae5b938e67
Mohamed Rahman Shareff S <62998470+rahmansk@users.noreply.github.com>
2025-06-07 17:35:00 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('appdb', 'admin', 'Admin1234', {
-  host: 'terraform-20250604105121162000000001.c27ss8cu656n.us-east-1.rds.amazonaws.com',
+  host: 'terraform-20250607053856175300000001.c27ss8cu656n.us-east-1.rds.amazonaws.com',
   port: 3306,
   dialect: 'mysql',
   logging: console.log // (or false to hide logs)
